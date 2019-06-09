5efdd6b1f3879886fbf4f5d474fe86ee3837e5eb
IgorSSK <igorsouza.96@hotmail.com>
2019-06-09 14:50:22 -0300

    Hunt Web Done

---
+++ huntweb/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header/index'
+import Main from './pages/main/index'
+import Routes from './routes'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Routes/>
+    </div>
+  );
+}
+
+export default App;
